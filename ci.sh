65cf04a79d226ec457697af0e145112232d63839
João Xavier <joao.xavier@gameduell.de>
2015-06-02 20:08:29 +0200

    fixed ci / test script

--- ci.sh
+++ ci.sh
@@ -1,15 +1,22 @@
 #!/bin/sh
 set -e
 
-rm -rf tests
-mkdir tests
-cd tests
+haxelib remove duell_sample || true > /dev/null
+rm -rf tmp
+
+mkdir tmp
+cd tmp
 haxelib run duell_duell create libraryProject
+
+haxelib dev duell_sample .
+
+cd tests
 
 haxelib run duell_duell build android -test -verbose -D jenkins -yestoall -emulator -x86
 haxelib run duell_duell build ios -test -verbose -D jenkins -yestoall
 haxelib run duell_duell build html5 -test -verbose -D jenkins -yestoall
 haxelib run duell_duell build flash -test -verbose -D jenkins -yestoall
 
-cd ..
-rm -rf tests
+cd ../..
+rm -rf tmp
+haxelib remove duell_sample
