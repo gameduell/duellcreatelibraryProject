ca30c01c61a0864a5b90f84b0b341de6480897b9
João Xavier <joao.xavier@gameduell.de>
2015-06-02 20:29:47 +0200

    Added update to the ci plugin and removed the copyright from the java header

--- ci.sh
+++ ci.sh
@@ -12,9 +12,10 @@ haxelib dev duell_sample .
 
 cd tests
 
+haxelib run duell_duell update -yestoall
+haxelib run duell_duell build html5 -test -verbose -D jenkins -yestoall
 haxelib run duell_duell build android -test -verbose -D jenkins -yestoall -emulator -x86
 haxelib run duell_duell build ios -test -verbose -D jenkins -yestoall
-haxelib run duell_duell build html5 -test -verbose -D jenkins -yestoall
 haxelib run duell_duell build flash -test -verbose -D jenkins -yestoall
 
 cd ../..
--- template/backends/sample_android/java/com/test/ExternalInterface.java
+++ template/backends/sample_android/java/com/test/ExternalInterface.java
@@ -1,7 +1,3 @@
-/*
- * Copyright (c) 2003-2015 GameDuell GmbH, All Rights Reserved
- * This document is strictly confidential and sole property of GameDuell GmbH, Berlin, Germany
- */
 package com.test;
 
 public class ExternalInterface
